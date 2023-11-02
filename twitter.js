89c876c9997240172ec6c3d757b2a2bd056120d6
Agustina Pucharras <pucharra81@gmail.com>
2023-11-01 21:57:58 -0300

    comentarios de ejercicio twitter

--- twitter.js
+++ twitter.js
@@ -1,17 +1,17 @@
 function enviarTweet(event) {
     event.preventDefault();
-    const tweet = document.getElementById('tweet');
-    const tweetText = tweet.value;
-    const newTweet = document.createElement('div');
-    const idTweet = new Date().getTime();
-    newTweet.id = idTweet;
-    newTweet.innerHTML = `
+    const tweet = document.getElementById('tweet'); //traigo al textarea
+    const tweetText = tweet.value; //obtengo el valor del twxtarea y lo guardo
+    const newTweet = document.createElement('div'); //creo un div para cada tweet
+    const idTweet = new Date().getTime(); //creo ids
+    newTweet.id = idTweet; //asigno id a cada div
+    newTweet.innerHTML = ` 
     <p>${tweetText}</p>
     <button class="btn btn-danger d-block ms-auto" onclick="eliminarTweet(${idTweet})">Eliminar tweet</button>
     `
     newTweet.classList.add("text-light", "bg-dark", "p-5", "my-2");
-    const tweetPadre = document.querySelector(".contenedor-tweet");
-    tweetPadre.appendChild(newTweet);
+    const tweetPadre = document.querySelector(".contenedor-tweet"); //traigo al contenedor del html
+    tweetPadre.appendChild(newTweet); //pongo adentro de ese div a cada tweet
 
     document.querySelector("form").reset(); //elimino lo q ya esta escrito en el form
 }
